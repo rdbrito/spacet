40e2637c587582e82b68c7cc68018653e5bf9b56
rdbrito <rdbrito50@gmail.com>
2018-10-15 17:11:10 -0300

    Add files via upload

---
+++ spacet/header.php
@@ -0,0 +1,24 @@
+<?php
+    //===========================
+    //cabeçalho
+    //===========================
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Spacet</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="inc/main.css">
+    <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+</head>
+<body>
+    
+
+
+
+   
---
+++ spacet/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    //===========================
+    //index
+    //===========================
+    include_once('header.php');
+
+    echo '<h2>Teste de font-awesome <sapn class="fa fa-trash"></span></h2>';
+    include_once('footer.php');
+
+?>
